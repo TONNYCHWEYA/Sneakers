c578e0380e494a80ad8a5a9afc8aa0bbddce62fc
TONNYCHWEYA <chweyatonny@gmail.com>
2022-08-01 11:56:46 +0300

    About page updated

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -13,6 +13,15 @@ export default function About() {
     <h2>About Sneaker shop</h2>
     <p>Sneaker shop is a shop where you will buy and sell shoes. You will be able to sell shoes that are both  new and old. 
     Content may not be copied, reproduced, distributed, displayed, downloaded or otherwise used for any purpose without the prior written consent of owners
+     </p>
+     <p>
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum enim augue, interdum nec sapien eget, pellentesque eleifend neque. Sed ullamcorper leo quis leo ullamcorper, ut finibus diam sagittis. Nam vehicula, nulla eget efficitur imperdiet, orci mauris faucibus diam, ut eleifend sem lacus at quam. Morbi accumsan ornare arcu, eget vestibulum enim. Sed lobortis ligula eu neque dictum, ac dapibus ex pulvinar. Aliquam a egestas lectus, eu efficitur ex. Maecenas tincidunt auctor tempor. Mauris semper, purus et lacinia suscipit, libero est lacinia nisi, eu malesuada ligula mi at metus. Donec sapien purus, tempus vel bibendum eu, consequat eget lacus.
+    </p>
+    <p>
+    Proin nec ex hendrerit, sagittis libero eget, mollis magna. Integer scelerisque erat sit amet iaculis egestas. Pellentesque auctor arcu odio, eu mollis velit dictum at. Donec imperdiet neque non mollis malesuada. Nulla pharetra diam turpis. Pellentesque tristique mauris et scelerisque tempus. Fusce ultrices dapibus nulla, ac pulvinar risus vestibulum eget. Nullam ullamcorper mi non est pretium vestibulum. In quis mollis velit, nec commodo libero. Curabitur in sapien sapien. Sed congue libero enim, eget aliquet erat cursus vestibulum.
+    </p>
+    <p>
+    Fusce feugiat porta magna ut malesuada. Vivamus semper lobortis arcu placerat dictum. Vivamus ullamcorper augue at lorem accumsan, vitae pharetra mauris luctus. Suspendisse nunc enim, venenatis ut mi sit amet, placerat tempor tortor. Vivamus ut lacus augue. Morbi tempor lectus id vestibulum vehicula. Cras vehicula blandit eleifend. Ut tincidunt ut tortor at fermentum. Curabitur id metus eget risus fermentum tempor. Nunc urna felis, posuere sed blandit vitae, laoreet sit amet ante. Sed sed dolor augue. Ut bibendum nunc id nibh tristique hendrerit. Vivamus a scelerisque augue.
     </p>
    </Center>
   )
